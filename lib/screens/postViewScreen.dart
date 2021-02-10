6924399b1dd4499f04c9c04c498312c3466b1b01
Noob Scorpio <aliadam7867@gmail.com>
2021-02-10 10:44:54 +0500

    First Commit

--- lib/screens/postViewScreen.dart
+++ lib/screens/postViewScreen.dart
@@ -19,114 +19,208 @@ class _PostViewScreenState extends State<PostViewScreen> {
             padding: const EdgeInsets.all(15),
             child: Column(
               children: [
-                Container(
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Row(
-                            children: [
-                              CircleAvatar(
-                                  radius: 30.0,
-                                  backgroundImage: AssetImage(
-                                      'assets/images/advabeauty.png')),
-                              SizedBox(
-                                width: 15,
-                              ),
-                              Text('John Doe'),
-                            ],
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 8.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [Text('Today'), Text('12:45 PM')],
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Container(
-                        child: Image.asset('assets/images/advabeauty.png'),
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(left: 20.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text('Share'),
-                                SizedBox(
-                                  width: 10,
-                                ),
-                                Icon(
-                                  Icons.share,
-                                  color: primaryColor,
-                                ),
-                              ],
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 15),
-                            child: Container(
-                              width: 1,
-                              height: 25,
-                              color: Colors.black,
-                            ),
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text('Comments: 30'),
-                              SizedBox(
-                                width: 10,
-                              ),
-                              Image.asset('assets/images/comment.png'),
-                            ],
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.symmetric(horizontal: 15),
-                            child: Container(
-                              width: 1,
-                              height: 25,
-                              color: Colors.black,
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.only(right: 8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              children: [
-                                Text('Likes: 30'),
-                                SizedBox(
-                                  width: 10,
-                                ),
-                                Icon(
-                                  Icons.thumb_up,
-                                  color: primaryColor,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+                Post(
+                    userImage: 'assets/images/advabeauty.png',
+                    userName: 'John Doe',
+                    date: 'Today',
+                    time: '12:45 PM',
+                    comments: '30',
+                    likes: '30',
+                    bodyImage: 'assets/images/advabeauty.png'),
+                SizedBox(
+                  height: 20,
                 ),
+                Comment(
+                    image: 'assets/images/advabeauty.png',
+                    body:
+                        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut',
+                    name: 'John Doe'),
+                Comment(
+                    image: 'assets/images/advabeauty.png',
+                    body:
+                        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut',
+                    name: 'John Doe'),
+                Comment(
+                    image: 'assets/images/advabeauty.png',
+                    body:
+                        'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut',
+                    name: 'John Doe'),
               ],
             ),
           ),
         ],
       ),
+    );
+  }
+}
+
+class Post extends StatelessWidget {
+  final userImage, userName, date, time, comments, likes, bodyImage;
+  const Post({
+    Key key,
+    this.userImage,
+    this.userName,
+    this.date,
+    this.time,
+    this.comments,
+    this.likes,
+    this.bodyImage,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                      radius: 30.0, backgroundImage: AssetImage(userImage)),
+                  SizedBox(
+                    width: 15,
+                  ),
+                  Text(
+                    userImage,
+                    style: boldTextStyle,
+                  ),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [Text('Today'), Text('12:45 PM')],
+                ),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            child: Image.asset(bodyImage),
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Share'),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Icon(
+                      Icons.share,
+                      color: primaryColor,
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Container(
+                  width: 1,
+                  height: 25,
+                  color: Colors.black,
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Comments: $comments'),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Image.asset('assets/images/comment.png'),
+                ],
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 15),
+                child: Container(
+                  width: 1,
+                  height: 25,
+                  color: Colors.black,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Likes: 30'),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Icon(
+                      Icons.thumb_up,
+                      color: primaryColor,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Comment extends StatelessWidget {
+  final image, name, body;
+  const Comment({
+    Key key,
+    this.image,
+    this.name,
+    this.body,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        CircleAvatar(radius: 25.0, backgroundImage: AssetImage(image)),
+        SizedBox(
+          width: 10,
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(top: 10.0),
+            child: Container(
+              width: double.maxFinite,
+              color: commentBGColor,
+              height: 75,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    name,
+                    style: boldTextStyle,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    body,
+                    style: normalTextStyle,
+                  )
+                ],
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,7 +5,9 @@ const sideContainerColor = Color(0xFFF5F5F5);
 const cartTextColor = Color(0xFFB3B3B3);
 const cartContainerColor = Color(0xFFF6F6F6);
 const checkoutButtonColor = Color(0xFF4369D3);
-
+const commentBGColor = Color(0xFFF8F8F8);
+const boldTextStyle = TextStyle(fontSize: 15, fontWeight: FontWeight.normal);
+const normalTextStyle = TextStyle(fontSize: 12);
 const List<String> categoryListStrings = [
   'Fashion',
   'Lenses',
